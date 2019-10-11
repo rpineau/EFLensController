f5a9e9a599fdfcbe1e87412e08a542b616448289
Rodolphe Pineau <pineau@rti-zone.org>
2019-10-10 19:16:23 -0700

    Uprev to version 1.2

--- x2focuser.h
+++ x2focuser.h
@@ -33,7 +33,7 @@ class MutexInterface;
 class BasicIniUtilInterface;
 class TickCountInterface;
 
-#define DRIVER_VERSION      1.1
+#define DRIVER_VERSION      1.2
 
 #define PARENT_KEY			"EFCTL"
 #define CHILD_KEY_PORTNAME	"PortName"
